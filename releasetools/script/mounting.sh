904b26c0a9df8325ce52e7f71b9536f2adca673b
Simone Renzo <simone201@gmail.com>
2011-09-06 18:55:17 +0200

    Fixed PATHs in mounting script + remove SH Updater if found (doesn't work under CWM 3/4)

--- releasetools/script/mounting.sh
+++ releasetools/script/mounting.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
-#Detecting kernel and partitions
+#!/system/bin/sh
+# Detecting kernel and partitions
+
 if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 
 	# Mounting System
-	if [ $(/sbin/mount | grep "on /system type" | wc -l) -eq "1" ]; then
+	if [ $(/system/bin/mount | grep "on /system type" | wc -l) -eq "1" ]; then
 		echo "SYSTEM is mounted"
 		umount /system
-		if [ $(/sbin/mount | grep "/dev/block/stl9 on /system" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/stl9 on /system" | wc -l) -eq "1" ]; then
 			echo "SYSTEM is NOT successfully unmounted"
 		else
 			echo "SYSTEM is successfully ummounted"
@@ -16,16 +17,16 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 	echo "mounting SYSTEM"
 	if [ $(cat /voodoo/run/cwm_recovery.fstab | grep "/system ext4 /dev/block/stl9" | wc -l) -eq "1" ]; then
 		echo "mounting SYSTEM as EXT4"
-		/sbin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl9 /system
-		if [ $(/sbin/mount | grep "/dev/block/stl9 on /system type ext4" | wc -l) -eq "1" ]; then
+		/system/bin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl9 /system
+		if [ $(/system/bin/mount | grep "/dev/block/stl9 on /system type ext4" | wc -l) -eq "1" ]; then
 			echo "SYSTEM is successfully mounted"
 		else
 			echo "SYSTEM is NOT successfully mounted"
 		fi
 	else
 		echo "mounting SYSTEM as RFS"
-		/sbin/mount -o rw /system
-		if [ $(/sbin/mount | grep "/dev/block/stl9 on /system type rfs" | wc -l) -eq "1" ]; then
+		/system/bin/mount -o rw /system
+		if [ $(/system/bin/mount | grep "/dev/block/stl9 on /system type rfs" | wc -l) -eq "1" ]; then
 			echo "SYSTEM is successfully mounted"
 		else
 			echo "SYSTEM is NOT successfully mounted"
@@ -33,10 +34,10 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 	fi
 
 	# Mounting Data
-	if [ $(/sbin/mount | grep "on /data type" | wc -l) -eq "1" ]; then
+	if [ $(/system/bin/mount | grep "on /data type" | wc -l) -eq "1" ]; then
 		echo "DATA is mounted"
 		umount /data
-		if [ $(/sbin/mount | grep "/dev/block/mmcblk0p2 on /data" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/mmcblk0p2 on /data" | wc -l) -eq "1" ]; then
 			echo "DATA is NOT successfully unmounted"
 		else
 			echo "DATA is successfully ummounted"
@@ -45,16 +46,16 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 	echo "mounting DATA"
 	if [ $(cat /voodoo/run/cwm_recovery.fstab | grep "/data ext4 /dev/block/mmcblk0p2" | wc -l) -eq "1" ]; then
 		echo "mounting DATA as EXT4"
-		/sbin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/mmcblk0p2 /data
-		if [ $(/sbin/mount | grep "/dev/block/mmcblk0p2 on /data type ext4" | wc -l) -eq "1" ]; then
+		/system/bin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/mmcblk0p2 /data
+		if [ $(/system/bin/mount | grep "/dev/block/mmcblk0p2 on /data type ext4" | wc -l) -eq "1" ]; then
 			echo "DATA is successfully mounted"
 		else
 			echo "DATA is NOT successfully mounted"
 		fi
 	else
 		echo "mounting DATA as RFS"
-		/sbin/mount -o rw /data
-		if [ $(/sbin/mount | grep "/dev/block/mmcblk0p2 on /data type rfs" | wc -l) -eq "1" ]; then
+		/system/bin/mount -o rw /data
+		if [ $(/system/bin/mount | grep "/dev/block/mmcblk0p2 on /data type rfs" | wc -l) -eq "1" ]; then
 			echo "DATA is successfully mounted"
 		else
 			echo "DATA is NOT successfully mounted"
@@ -62,10 +63,10 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 	fi
 
 	# Mounting DBData
-	if [ $(/sbin/mount | grep "on /datadata type" | wc -l) -eq "1" ]; then
+	if [ $(/system/bin/mount | grep "on /datadata type" | wc -l) -eq "1" ]; then
 		echo "DBDATA is mounted"
 		umount /datadata
-		if [ $(/sbin/mount | grep "/dev/block/stl10 on /datadata" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/stl10 on /datadata" | wc -l) -eq "1" ]; then
 			echo "DBDATA is NOT successfully unmounted"
 		else
 			echo "DBDATA is successfully ummounted"
@@ -74,8 +75,8 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 	echo "mounting DBDATA"
 	if [ $(cat /voodoo/run/cwm_recovery.fstab | grep "/datadata ext4 /dev/block/stl10" | wc -l) -eq "1" ]; then
 		echo "mounting DBDATA as EXT4"
-		/sbin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl10 /datadata
-		if [ $(/sbin/mount | grep "/dev/block/stl10 on /data type ext4" | wc -l) -eq "1" ]; then
+		/system/bin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl10 /datadata
+		if [ $(/system/bin/mount | grep "/dev/block/stl10 on /data type ext4" | wc -l) -eq "1" ]; then
 			echo "DBDATA is successfully mounted"
 		else
 			echo "DBDATA is NOT successfully mounted"
@@ -83,7 +84,7 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 	else
 		echo "mounting DBDATA as RFS"
 		mount /datadata
-		if [ $(/sbin/mount | grep "/dev/block/stl10 on /datadata type rfs" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/stl10 on /datadata type rfs" | wc -l) -eq "1" ]; then
 			echo "DBDATA is successfully mounted"
 		else
 			echo "DBDATA is NOT successfully mounted"
@@ -109,7 +110,7 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 
 	if [ -d "/sdcard/Voodoo" ]; then
 		echo "Deleting old Voodoo Folder"
-		/sbin/rm -Rf "/sdcard/Voodoo"
+		/system/bin/rm -Rf "/sdcard/Voodoo"
 		if [ -e "/sdcard/Voodoo" ]; then
 			echo "Old Voodoo Folder was not deleted"
 		else
@@ -122,13 +123,13 @@ if [ -f "/voodoo/run/cwm_recovery.fstab" ]; then
 
 	if [ $lagfix -eq "0" ]; then
 		echo "Disabling Voodoo Lagfix"
-		/sbin/touch "/sdcard/Voodoo/disable-lagfix"
+		/system/xbin/touch "/sdcard/Voodoo/disable-lagfix"
 	fi
 
 	if [ $sys_lagfix -eq "0" ]; then
 		echo "Disabling System Lagfix"
-		/sbin/touch "/sdcard/Voodoo/system-as-rfs"
-		/sbin/touch "/sdcard/Voodoo/dont-convert-system"
+		/system/xbin/touch "/sdcard/Voodoo/system-as-rfs"
+		/system/xbin/touch "/sdcard/Voodoo/dont-convert-system"
 	fi
 	
 else
@@ -137,7 +138,7 @@ else
 	#setup voodoo
 	if [ -d "/sdcard/Voodoo" ]; then
 		echo "Deleting old Voodoo Folder"
-		/sbin/rm -Rf "/sdcard/Voodoo"
+		/system/bin/rm -Rf "/sdcard/Voodoo"
 		if [ -e "/sdcard/Voodoo" ]; then
 			echo "Old Voodoo Folder was not deleted"
 		else
@@ -149,14 +150,14 @@ else
 	mkdir "/sdcard/Voodoo"
 	
 	# check and mount dbdata
-	if [ $(/sbin/mount | grep "/dev/block/stl10 on /dbdata" | wc -l) -eq "1" ]; then
+	if [ $(/system/bin/mount | grep "/dev/block/stl10 on /dbdata" | wc -l) -eq "1" ]; then
 		echo "DBDATA is mounted at /dbdata"
 		if [ -e "/datadata" ]; then
-			/sbin/rm -Rf /datadata
+			/system/bin/rm -Rf /datadata
 		fi	
 		ln -s /dbdata /datadata
 	else
-		if [ $(/sbin/mount | grep "/dev/block/stl10 on /datadata" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/stl10 on /datadata" | wc -l) -eq "1" ]; then
 			echo "DBDATA is mounted at /datadata"
 		else
 			echo "DBDATA is NOT mounted"
@@ -168,8 +169,8 @@ else
 			fi
 			if tune2fs -l /dev/block/stl10 > /dev/null; then
 				echo "mounting DBDATA as EXT4"
-				/sbin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl10 /datadata
-				if [ $(/sbin/mount | grep "/dev/block/stl10 on /data type ext4" | wc -l) -eq "1" ]; then
+				/system/bin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl10 /datadata
+				if [ $(/system/bin/mount | grep "/dev/block/stl10 on /data type ext4" | wc -l) -eq "1" ]; then
 					echo "DBDATA is successfully mounted"
 				else
 					echo "DBDATA is NOT successfully mounted"
@@ -177,7 +178,7 @@ else
 			else
 				echo "mounting DBDATA as RFS"
 				mount -o rw /datadata
-				if [ $(/sbin/mount | grep "/dev/block/stl10 on /datadata type rfs" | wc -l) -eq "1" ]; then
+				if [ $(/system/bin/mount | grep "/dev/block/stl10 on /datadata type rfs" | wc -l) -eq "1" ]; then
 					echo "DBDATA is successfully mounted"
 				else
 					echo "DBDATA is NOT successfully mounted"
@@ -187,64 +188,64 @@ else
 	fi
 	
 	# check and mount data
-	if [ $(/sbin/mount | grep "on /data type" | wc -l) -eq "1" ]; then
+	if [ $(/system/bin/mount | grep "on /data type" | wc -l) -eq "1" ]; then
 		echo "DATA is mounted"
-		if [ $(/sbin/mount | grep "/dev/block/mmcblk0p2 on /data type rfs" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/mmcblk0p2 on /data type rfs" | wc -l) -eq "1" ]; then
 			echo "Disabling Voodoo Lagfix"
-			/sbin/touch "/sdcard/Voodoo/disable-lagfix"
+			/system/xbin/touch "/sdcard/Voodoo/disable-lagfix"
 		fi
 	else
 		echo "mounting DATA"
 		if tune2fs -l /dev/block/mmcblk0p2 > /dev/null; then
 			echo "mounting DATA as EXT4"
-			/sbin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/mmcblk0p2 /data
-			if [ $(/sbin/mount | grep "/dev/block/mmcblk0p2 on /data type ext4" | wc -l) -eq "1" ]; then
+			/system/bin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/mmcblk0p2 /data
+			if [ $(/system/bin/mount | grep "/dev/block/mmcblk0p2 on /data type ext4" | wc -l) -eq "1" ]; then
 				echo "DATA is successfully mounted"
 			else
 				echo "DATA is NOT successfully mounted"
 			fi
 		else
 			echo "mounting DATA as RFS"
-			/sbin/mount -o rw /data
-			if [ $(/sbin/mount | grep "/dev/block/mmcblk0p2 on /data type rfs" | wc -l) -eq "1" ]; then
+			/system/bin/mount -o rw /data
+			if [ $(/system/bin/mount | grep "/dev/block/mmcblk0p2 on /data type rfs" | wc -l) -eq "1" ]; then
 				echo "DATA is successfully mounted"
 			else
 				echo "DATA is NOT successfully mounted"
 			fi
 			echo "Disabling Voodoo Lagfix"
-			/sbin/touch "/sdcard/Voodoo/disable-lagfix"
+			/system/xbin/touch "/sdcard/Voodoo/disable-lagfix"
 		fi
 	fi
 	
 	# check and mount system
-	if [ $(/sbin/mount | grep "on /system type" | wc -l) -eq "1" ]; then
+	if [ $(/system/bin/mount | grep "on /system type" | wc -l) -eq "1" ]; then
 		echo "SYSTEM is mounted"
-		if [ $(/sbin/mount | grep "/dev/block/stl9 on /system type rfs" | wc -l) -eq "1" ]; then
+		if [ $(/system/bin/mount | grep "/dev/block/stl9 on /system type rfs" | wc -l) -eq "1" ]; then
 			echo "Disabling System Lagfix"
-			/sbin/touch "/sdcard/Voodoo/system-as-rfs"
-			/sbin/touch "/sdcard/Voodoo/dont-convert-system"
+			/system/xbin/touch "/sdcard/Voodoo/system-as-rfs"
+			/system/xbin/touch "/sdcard/Voodoo/dont-convert-system"
 		fi
 	else
 		echo "mounting SYSTEM"
 		if [ tune2fs -l /dev/block/stl9 > /dev/null; ]; then
 			echo "mounting SYSTEM as EXT4"
-			/sbin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl9 /system
-			if [ $(/sbin/mount | grep "/dev/block/stl9 on /system type ext4" | wc -l) -eq "1" ]; then
+			/system/bin/mount -t ext4 -o rw,noatime,barrier=0,data=writeback,noauto_da_alloc /dev/block/stl9 /system
+			if [ $(/system/bin/mount | grep "/dev/block/stl9 on /system type ext4" | wc -l) -eq "1" ]; then
 				echo "SYSTEM is successfully mounted"
 			else
 				echo "SYSTEM is NOT successfully mounted"
 			fi
 		else
 			echo "mounting SYSTEM as RFS"
-			/sbin/mount -o rw /system
-			if [ $(/sbin/mount | grep "/dev/block/stl9 on /system type rfs" | wc -l) -eq "1" ]; then
+			/system/bin/mount -o rw /system
+			if [ $(/system/bin/mount | grep "/dev/block/stl9 on /system type rfs" | wc -l) -eq "1" ]; then
 				echo "SYSTEM is successfully mounted"
 			else
 				echo "SYSTEM is NOT successfully mounted"
 			fi
 			echo "Disabling System Lagfix"
-			/sbin/touch "/sdcard/Voodoo/system-as-rfs"
-			/sbin/touch "/sdcard/Voodoo/dont-convert-system"
+			/system/xbin/touch "/sdcard/Voodoo/system-as-rfs"
+			/system/xbin/touch "/sdcard/Voodoo/dont-convert-system"
 		fi
 	fi
 fi
